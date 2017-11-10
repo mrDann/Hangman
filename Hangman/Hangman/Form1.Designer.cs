6acc8cce596211a0fd1925f1bf568a40dffeef9b
mrDann <32540565+mrDann@users.noreply.github.com>
2017-11-10 17:22:46 +0100

    Add files via upload

---
+++ Hangman/Hangman/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Location = new System.Drawing.Point(817, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(423, 558);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hangman";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(417, 528);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(785, 558);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(509, 489);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Woord lengte:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(12, 588);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1225, 171);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(34, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 31);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(514, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(453, 31);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 126);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Voeg letter toe";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1031, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 126);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Raad het woord";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1252, 771);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "HangMan - Dan(n)y";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Hangman/Hangman/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Black, 10);
+
+            g.DrawLine(p, new Point(130, 218), new Point(130, 5));
+            g.DrawLine(p, new Point(135, 5), new Point(65, 5));
+            g.DrawLine(p, new Point(60, 0), new Point(60, 50));
+            drawnBodyParts(bodyParts.Head);
+            drawnBodyParts(bodyParts.Left_eye);
+            drawnBodyParts(bodyParts.Right_eye);
+            drawnBodyParts(bodyParts.Mouth);
+            drawnBodyParts(bodyParts.body);
+            drawnBodyParts(bodyParts.Left_arm);
+            drawnBodyParts(bodyParts.Right_arm);
+            drawnBodyParts(bodyParts.Left_leg);
+            drawnBodyParts(bodyParts.Right_leg);
+        }
+
+        enum bodyParts
+        {
+            Head,
+            Left_eye,
+            Right_eye,
+            Mouth,
+            Right_arm,
+            Left_arm,
+            body,
+            Right_leg,
+            Left_leg
+        }
+
+        void drawnBodyParts(bodyParts bp) {
+
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Blue, 2);
+            if (bp == bodyParts.Head)
+                g.DrawEllipse(p, 40, 50, 40, 40);
+            else if(bp == bodyParts.Left_eye)
+            {
+                SolidBrush s = new SolidBrush(Color.Black);
+                g.FillEllipse(s, 50, 60, 5, 5);
+            }else if(bp == bodyParts.Right_eye)
+            {
+                SolidBrush s = new SolidBrush(Color.Black);
+                g.FillEllipse(s, 63, 60, 5, 5);
+            }else if (bp == bodyParts.Mouth)
+            {
+                g.DrawArc(p, 50, 60, 20, 20, 45, 100);
+            }else if (bp == bodyParts.body)
+            {
+                g.DrawLine(p, new Point(60, 90), new Point(60, 170));
+            }else if(bp == bodyParts.Left_arm)
+            {
+                g.DrawLine(p, new Point(60, 100), new Point(30, 85));
+            }
+            else if(bp == bodyParts.Right_arm)
+            {
+                g.DrawLine(p, new Point(60, 100), new Point(90, 85));
+
+            }else if(bp == bodyParts.Left_leg){
+                g.DrawLine(p, new Point(60, 170), new Point(30, 190));
+            }
+            else if(bp == bodyParts.Right_leg)
+            {
+                g.DrawLine(p, new Point(60, 170), new Point(90, 190));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+     
+    }
+}
